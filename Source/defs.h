ae4c2638b26b0703fd6b6dd42681cf73904ee38a
hobbes543 <39176746+hobbes543@users.noreply.github.com>
2024-03-07 19:28:08 +0000

    Part 02 Parser

--- Source/defs.h
+++ Source/defs.h
@@ -5,11 +5,24 @@
 
 // Tokens
 enum {
-    T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INTLIT
+    T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INTLIT, T_EOF
 };
 
 // Token Structure
 struct token {
     int token;
     int intvalue;
+};
+
+// AST node types
+enum {
+    A_ADD, A_SUBTRACT, A_MULTIPLY, A_DIVIDE, A_INTLIT
+};
+
+// Abstract Sytax Tree structure
+struct ASTnode{
+    int op;                         // "Operation to be performed"
+    struct ASTnode *left;           // Left and right child trees
+    struct ASTnode *right;
+    int intvalue;                   // For A_INTLIT, the integer value    
 };
--- Source/main.c
+++ Source/main.c
@@ -17,26 +17,12 @@ static void usage(char *prog) {
     exit(1);
 }
 
-// List of printable tokens
-char *tokenstr[] = { "+", "-", "*", "/", "intlit" };
-
-// Loop scanning in all the tokens in the input file.
-// Print out details of each token found.
-static void scanfile() {
-    struct token T;
-
-    while (scan(&T)) {
-        printf("Token %s", tokenstr[T.token]);
-        if (T.token == T_INTLIT)
-            printf(", value %d", T.intvalue);
-        printf("\n");
-    }
-}
-
 // Main program: check arguments and print a usage
 // if we don't have an arguement.  Open up the input
 // file and call scanfile() to scan the tokens in it.
 void main(int argc, char *argv[]) {
+    struct ASTnode *n;
+    
     if (argc != 2)
         usage(argv[0]);
     
@@ -47,6 +33,8 @@ void main(int argc, char *argv[]) {
         exit(1);
     }
 
-    scanfile();
+    scan(&Token);           // Get the first token from the input
+    n = binexpr();          // Parse the expression in the file
+    printf("%d\n", interpretAST(n));    // Calculate the final result   
     exit(0);
 }
